ae7a55f3b7ad86e841e88f4e788c6d2b71bb378e
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2020-03-29 13:25:28 -0700

    added server-side code

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(express.static("./public"));
+
+var MONGODB_URI =
+    process.env.MONGODB_URI || "mongodb://localhost/mongoFlights";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
